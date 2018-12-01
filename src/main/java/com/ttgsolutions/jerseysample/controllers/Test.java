7ab5841496edb977e267bb90242a6bf1365ea1c2
James Rice <James.Rice@ttg-solutions.com>
2018-12-01 16:22:11 -0500

    Updated gradle to support jdk 11 from tomcat.

--- src/main/java/com/ttgsolutions/jerseysample/controllers/Test.java
+++ src/main/java/com/ttgsolutions/jerseysample/controllers/Test.java
@@ -2,7 +2,9 @@ package com.ttgsolutions.jerseysample.controllers;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -12,8 +14,14 @@ import javax.ws.rs.core.Response;
 public class Test {
 
     @GET
-    @Produces("text/plain")
-    public Response getText() {
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getTest() {
         return Response.ok("Hello world!\n").build();
     }
+
+//    @GET @Path("{id}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response getManyTest(@PathParam("id") String id) {
+//        return Response.ok("Hello world!\n").build();
+//    }
 }
